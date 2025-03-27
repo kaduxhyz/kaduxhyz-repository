99f72b21b69d2c8cf233b993ca486a0a222ae0a1
kaduxhyz <108536573+kaduxhyz@users.noreply.github.com>
2025-03-27 12:16:16 -0300

    update

---
+++ JavaScript/calculoMedia.js
@@ -0,0 +1,25 @@
+//função somar 
+function somar(num1,num2,num3){
+    let soma = (num1,num2,num3);
+    return(console.log('a soma dos numeros ${soma}'))
+    //nan not a number 
+    
+  }
+  
+  //função media 
+  function media(){
+    //array notas
+    let notas = [10,10,10,10,10]
+    //variavel para receber o valor das notas
+    let soma = 0;
+  }
+  //calculo media 
+  function media(nota1,nota2,nota3){
+    var nota1 = number(prompt('qual nota 1?'))
+    var nota2 = number(prompt('qual nota 2?'))
+    var nota3 = number(prompt('qual nota 3?'))
+    mediaNotas((nota1+nota2+nota3)/3)
+    alert()
+  }
+  
+  media()
