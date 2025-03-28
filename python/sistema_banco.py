0dd9f3f7df926ab772ea95be13408b6f52ecb2a6
kaduxhyz <108536573+kaduxhyz@users.noreply.github.com>
2025-03-28 11:16:00 -0300

    criação da função menu

---
+++ python/sistema_banco.py
@@ -0,0 +1,18 @@
+#print("**********Menu**********")
+#print("Criar Usuario")
+#print("Depositar")
+#print("Sacar")]
+
+def menu():
+    print('''
+        **********Menu**********
+        (1) Criar Usuario
+        (2) Criar Conta
+        (3) Listar Contas
+        (d) Depositar
+        (s) Sacar
+        (e) Estrato
+        (q) Sair
+    ''')
+    return input("Qual opção deseja?")
+menu()
